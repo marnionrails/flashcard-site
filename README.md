f4414fab040030e7e08deda566f377400362d94a
marnionrails <37088724+marnionrails@users.noreply.github.com>
2021-01-27 23:24:44 -0500

    transform webpage into git flashcards and add README

---
+++ README.md
@@ -0,0 +1,34 @@
+# Git Flashcards
+
+#### Flashcards to memorize git commands
+
+#### By **Marni Sucher**
+
+## Technologies Used
+
+* HTML
+* CSS
+* Bootstrap
+* jQuery
+* JavaScript
+
+## Description
+
+This webage consists of flashcards that will aid the user in memorizing git commands. The user simply clicks on the flashcard and its corresponding definition will appear. 
+
+## Setup/Installation Requirements
+
+* Clone https://github.com/marnionrails/flashcard-site
+* Open flashcard-site folder
+* Open index.html
+
+## Known Bugs
+
+* None at this time
+
+## GPL License
+
+## Contact Information
+
+suchermarni@gmail.com
+
--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,37 @@
+
 $(document).ready(function() {
-  $(".clickable").click(function() {
-    $(".initially-showing").toggle();
-    $(".initially-hidden").toggle();
+  $(".git").click(function() {
+    $(".git.front").slideToggle();
+    $(".git.back").slideToggle();
+  });
+
+  $(".help").click(function() {
+    $(".help.front").slideToggle();
+    $(".help.back").slideToggle();
+  });
+
+  $(".status").click(function() {
+    $(".status.front").slideToggle();
+    $(".status.back").slideToggle();
+  });
+
+  $(".reset").click(function() {
+    $(".reset.front").slideToggle();
+    $(".reset.back").slideToggle();
+  });
+
+  $(".amend").click(function() {
+    $(".amend.front").slideToggle();
+    $(".amend.back").slideToggle();
+  });
+
+  $(".name").click(function() {
+    $(".name.front").slideToggle();
+    $(".name.back").slideToggle();
+  });
+
+  $(".email").click(function() {
+    $(".email.front").slideToggle();
+    $(".email.back").slideToggle();
   });
 });
